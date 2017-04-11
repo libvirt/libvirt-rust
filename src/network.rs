38247a295c2e10175298255d0574536c67ecbda5
Sahid Orentino Ferdjaoui <sahid.ferdjaoui@redhat.com>
2017-04-11 10:56:31 -0400

    completing network.rs

--- src/network.rs
+++ src/network.rs
@@ -39,11 +39,17 @@ extern {
     fn virNetworkLookupByID(c: virConnectPtr, id: libc::c_int) -> virNetworkPtr;
     fn virNetworkLookupByName(c: virConnectPtr, id: *const libc::c_char) -> virNetworkPtr;
     fn virNetworkLookupByUUIDString(c: virConnectPtr, uuid: *const libc::c_char) -> virNetworkPtr;
+    fn virNetworkCreate(d: virNetworkPtr, flags: libc::c_uint) -> libc::c_int;
     fn virNetworkDestroy(d: virNetworkPtr) -> libc::c_int;
+    fn virNetworkUndefine(d: virNetworkPtr) -> libc::c_int;
+    fn virNetworkFree(d: virNetworkPtr) -> libc::c_int;
     fn virNetworkIsActive(d: virNetworkPtr) -> libc::c_int;
     fn virNetworkGetName(d: virNetworkPtr) -> *const libc::c_char;
+    fn virNetworkGetXMLDesc(d: virNetworkPtr, flags: libc::c_uint) -> *const libc::c_char;
 }
 
+pub type NetworkXMLFlags = self::libc::c_uint;
+pub const VIR_NETWORK_XML_INACTIVE:NetworkXMLFlags = 1;
 
 pub struct Network {
     pub d: virNetworkPtr
@@ -98,10 +104,47 @@ impl Network {
         }
     }
 
+    pub fn get_xml_desc(&self, flags:NetworkXMLFlags) -> Result<&str, Error> {
+        unsafe {
+            let xml = virNetworkGetXMLDesc(self.d, flags);
+            if xml.is_null() {
+                return Err(Error::new())
+            }
+            return Ok(str::from_utf8(
+                CStr::from_ptr(xml).to_bytes()).unwrap())
+        }
+    }
+
+    pub fn create(&self, flags: NetworkXMLFlags) -> Result<(), Error> {
+        unsafe {
+            if virNetworkCreate(self.d, flags) == -1 {
+                return Err(Error::new());
+            }
+            return Ok(());
+        }
+    }
 
     pub fn destroy(&self) -> Result<(), Error> {
         unsafe {
             if virNetworkDestroy(self.d) == -1 {
+                return Err(Error::new());
+            }
+            return Ok(());
+        }
+    }
+
+    pub fn undefine(&self) -> Result<(), Error> {
+        unsafe {
+            if virNetworkUndefine(self.d) == -1 {
+                return Err(Error::new());
+            }
+            return Ok(());
+        }
+    }
+
+    pub fn free(&self) -> Result<(), Error> {
+        unsafe {
+            if virNetworkFree(self.d) == -1 {
                 return Err(Error::new());
             }
             return Ok(());
