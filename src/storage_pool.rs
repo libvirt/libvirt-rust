52b0bc641f2ee0a05dabea07d0fe7dc93e1f7a74
Sahid Orentino Ferdjaoui <sahid.ferdjaoui@redhat.com>
2017-04-11 11:10:26 -0400

    completing storage_pools

--- src/storage_pool.rs
+++ src/storage_pool.rs
@@ -39,10 +39,25 @@ extern {
     fn virStoragePoolLookupByID(c: virConnectPtr, id: libc::c_int) -> virStoragePoolPtr;
     fn virStoragePoolLookupByName(c: virConnectPtr, id: *const libc::c_char) -> virStoragePoolPtr;
     fn virStoragePoolLookupByUUIDString(c: virConnectPtr, uuid: *const libc::c_char) -> virStoragePoolPtr;
+    fn virStoragePoolCreate(d: virStoragePoolPtr, flags: libc::c_uint) -> libc::c_int;
     fn virStoragePoolDestroy(d: virStoragePoolPtr) -> libc::c_int;
+    fn virStoragePoolUndefine(d: virStoragePoolPtr) -> libc::c_int;
+    fn virStoragePoolFree(d: virStoragePoolPtr) -> libc::c_int;
     fn virStoragePoolIsActive(d: virStoragePoolPtr) -> libc::c_int;
+    fn virStoragePoolIsPersistent(d: virStoragePoolPtr) -> libc::c_int;
     fn virStoragePoolGetName(d: virStoragePoolPtr) -> *const libc::c_char;
+    fn virStoragePoolGetXMLDesc(d: virStoragePoolPtr, flags: libc::c_uint) -> *const libc::c_char;
 }
+
+pub type StoragePoolXMLFlags = self::libc::c_uint;
+pub const VIR_STORAGE_POOL_XML_INACTIVE:StoragePoolXMLFlags = 1 << 0;
+
+pub type StoragePoolCreateFlags = self::libc::c_uint;
+pub const STORAGE_POOL_CREATE_NORMAL:StoragePoolCreateFlags = 0;
+pub const STORAGE_POOL_CREATE_WITH_BUILD: StoragePoolCreateFlags = 1 << 0;
+pub const STORAGE_POOL_CREATE_WITH_BUILD_OVERWRITE: StoragePoolCreateFlags = 1 << 1;
+pub const STORAGE_POOL_CREATE_WITH_BUILD_NO_OVERWRITE: StoragePoolCreateFlags = 1 << 2;
+
 
 
 pub struct StoragePool {
@@ -98,6 +113,25 @@ impl StoragePool {
         }
     }
 
+    pub fn get_xml_desc(&self, flags:StoragePoolXMLFlags) -> Result<&str, Error> {
+        unsafe {
+            let xml = virStoragePoolGetXMLDesc(self.d, flags);
+            if xml.is_null() {
+                return Err(Error::new())
+            }
+            return Ok(str::from_utf8(
+                CStr::from_ptr(xml).to_bytes()).unwrap())
+        }
+    }
+
+    pub fn create(&self, flags: StoragePoolCreateFlags) -> Result<(), Error> {
+        unsafe {
+            if virStoragePoolCreate(self.d, flags) == -1 {
+                return Err(Error::new());
+            }
+            return Ok(());
+        }
+    }
 
     pub fn destroy(&self) -> Result<(), Error> {
         unsafe {
@@ -108,12 +142,41 @@ impl StoragePool {
         }
     }
 
-    pub fn is_active(&self) -> Result<(), Error> {
+    pub fn undefine(&self) -> Result<(), Error> {
         unsafe {
-            if virStoragePoolIsActive(self.d) == -1 {
+            if virStoragePoolUndefine(self.d) == -1 {
                 return Err(Error::new());
             }
             return Ok(());
+        }
+    }
+
+    pub fn free(&self) -> Result<(), Error> {
+        unsafe {
+            if virStoragePoolFree(self.d) == -1 {
+                return Err(Error::new());
+            }
+            return Ok(());
+        }
+    }
+
+    pub fn is_active(&self) -> Result<bool, Error> {
+        unsafe {
+            let ret = virStoragePoolIsActive(self.d);
+            if ret == -1 {
+                return Err(Error::new());
+            }
+            return Ok(ret == 1);
+        }
+    }
+
+    pub fn is_persistent(&self) -> Result<bool, Error> {
+        unsafe {
+            let ret = virStoragePoolIsPersistent(self.d);
+            if ret == -1 {
+                return Err(Error::new());
+            }
+            return Ok(ret == 1);
         }
     }
 }
