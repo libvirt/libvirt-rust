f500f362fe7b2b0d7c1f8b5ec08b0037202a5041
Sahid Orentino Ferdjaoui <sahid.ferdjaoui@redhat.com>
2017-04-13 10:34:24 -0400

    simple tool to help looking at what is missing

---
+++ tools/api_tests.py
@@ -0,0 +1,57 @@
+# Just tool to help finding what is implemented and what is missing
+
+import os
+import glob
+import xml.etree.ElementTree
+
+
+LIBVIRT_API_FILE="/usr/share/libvirt/api/libvirt-api.xml"
+MY_PATH = os.path.dirname(os.path.realpath(__file__))
+SRC_PATH = MY_PATH + "/../src"
+
+def get_api_symbols(doc):
+    funcs = doc.iter('function')
+    macros = doc.iter('macro')
+    enums = doc.iter('enum')
+    return funcs, macros, enums
+
+def get_sources():
+    return glob.glob(SRC_PATH + "/*.rs")
+
+def match(el, content):
+    return content.find(el) >= 0
+
+def main():
+    doc = xml.etree.ElementTree.parse(LIBVIRT_API_FILE).getroot()
+
+    implemented = set([])
+    missing = set([])
+    for el in doc.iter('function'):
+        
+        if el.get('name').startswith('virConnect'): # What i'm looking for?
+            
+            status = False
+            for source in get_sources():
+                f = open(source)
+                if match(el.get('name'), f.read()):
+                    status = True
+                    break
+            if status:
+                implemented.add(el)
+            else:
+                missing.add(el)
+
+    print "missing: %s, implemented: %s" % (len(missing), len(implemented))
+    print "missing:"
+    for x in missing:
+        print x.attrib
+    #print "implemented:"
+    #for x in implemented:
+    #    print x.attrib
+        
+
+if __name__ == '__main__':
+    main()
+    
+    
+    
