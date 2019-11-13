28a803db7c3ad98be09da281dd26824ee0c06289
Andrea Bolognani <abologna@redhat.com>
2019-11-13 10:34:01 +0100

    api_tests.py: Fix all issues reported by flake8
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: Erik Skultety <eskultet@redhat.com>

--- tools/api_tests.py
+++ tools/api_tests.py
@@ -6,9 +6,10 @@ import xml.etree.ElementTree
 import sys
 
 
-LIBVIRT_API_FILE="/usr/share/libvirt/api/libvirt-api.xml"
+LIBVIRT_API_FILE = "/usr/share/libvirt/api/libvirt-api.xml"
 MY_PATH = os.path.dirname(os.path.realpath(__file__))
 SRC_PATH = MY_PATH + "/../src"
+
 
 def get_api_symbols(doc):
     funcs = doc.iter('function')
@@ -16,26 +17,28 @@ def get_api_symbols(doc):
     enums = doc.iter('enum')
     return funcs, macros, enums
 
+
 def get_sources():
     return glob.glob(SRC_PATH + "/*.rs")
 
+
 def match(el, content):
     return content.find(el) >= 0
+
 
 def main():
     filter_by = ""
     if len(sys.argv) > 1:
         filter_by = sys.argv[1]
-    
+
     doc = xml.etree.ElementTree.parse(LIBVIRT_API_FILE).getroot()
 
     implemented = set([])
     missing = set([])
     for el in doc.iter('function'):
 
-        
-        if el.get('name').startswith(filter_by): # What i'm looking for?
-            
+        if el.get('name').startswith(filter_by):  # What I'm looking for
+
             status = False
             for source in get_sources():
                 f = open(source)
@@ -51,13 +54,10 @@ def main():
     print("missing:")
     for x in missing:
         print(x.attrib)
-    #print "implemented:"
-    #for x in implemented:
-    #    print x.attrib
-        
+    # print "implemented:"
+    # for x in implemented:
+    #     print x.attrib
+
 
 if __name__ == '__main__':
     main()
-    
-    
-    
