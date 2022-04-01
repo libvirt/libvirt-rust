57cce287d3e4922300d74f0db7d9760a5afe2249
Wim de With <wf@dewith.io>
2022-04-01 18:37:32 +0200

    Remove symbols test
    This test is intended to test whether the libvirt symbols referenced in
    the extern "C" blocks are valid. However, we generate those dynamically,
    so it doesn't make much sense to test this anymore.
    
    Signed-off-by: Wim de With <wf@dewith.io>

--- tests/symbols/src/lib.rs
+++
@@ -1,9 +0,0 @@
-use virt::connect::Connect;
-
-/// Implement a C main function, with at least 1 reference to a libvirt function
-/// It doesn't need to succeed at runtime; only compile successfully
-#[no_mangle]
-extern "C" fn main() -> u32 {
-    Connect::open("any_driver").ok();
-    0
-}
