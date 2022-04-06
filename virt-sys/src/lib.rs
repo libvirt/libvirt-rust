603a92db57b6059094061ec7cc7135ae4acd7893
Wim de With <wf@dewith.io>
2022-04-06 11:21:37 +0200

    sys: Add type alias for generated type name
    To avoid having _virTypedParameter__bindgen_ty_1 spread throughout the
    main crate, we define an alias for it. This can be transparently used in
    the place of the original type name.
    
    Signed-off-by: Wim de With <wf@dewith.io>

--- virt-sys/src/lib.rs
+++ virt-sys/src/lib.rs
@@ -3,4 +3,6 @@
 // Bindgen generated tests dereference null pointers for struct layout testing.
 #![cfg_attr(test, allow(unknown_lints, deref_nullptr))]
 
+pub type _virTypedParameterValue = _virTypedParameter__bindgen_ty_1;
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
