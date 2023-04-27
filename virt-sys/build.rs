f2de3823c07e5ca573be5dfe444defad1459a589
Andrea Bolognani <abologna@redhat.com>
2023-04-27 18:59:32 +0200

    sys: Refactor build.rs
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- virt-sys/build.rs
+++ virt-sys/build.rs
@@ -16,7 +16,21 @@ fn main() {
 
 fn run() -> Result<(), Box<dyn Error>> {
     println!("cargo:rerun-if-changed=wrapper.h");
+
     let mut config = pkg_config::Config::new();
+
+    config
+        .atleast_version(LIBVIRT_VERSION)
+        .probe("libvirt")?;
+
+    if cfg!(feature = "qemu") {
+        config
+            .atleast_version(LIBVIRT_VERSION)
+            .probe("libvirt-qemu")?;
+    }
+
+    let bindgen_out_dir = PathBuf::from(env::var("OUT_DIR")?);
+    let bindgen_out_file = bindgen_out_dir.join("bindings.rs");
 
     let mut bindings = bindgen::builder()
         .header("wrapper.h")
@@ -29,23 +43,15 @@ fn run() -> Result<(), Box<dyn Error>> {
         .prepend_enum_name(false)
         .ctypes_prefix("::libc");
 
-    config
-        .atleast_version(LIBVIRT_VERSION)
-        .probe("libvirt")?;
-
     if cfg!(feature = "qemu") {
-        config.probe("libvirt-qemu")?;
-
         bindings = bindings
             .clang_arg("-DBINDGEN_USE_QEMU");
     }
 
-    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
-
     bindings
         .generate()
         .map_err(|_| String::from("could not generate bindings"))?
-        .write_to_file(out_dir.join("bindings.rs"))?;
+        .write_to_file(bindgen_out_file)?;
 
     Ok(())
 }
