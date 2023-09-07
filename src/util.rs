ead5bf346a818ac0c0323ca00b1ea0b337e2de94
Andrea Bolognani <abologna@redhat.com>
2023-09-07 17:01:49 +0100

    src: add helper for converting 'c_ulong' to 'u64'
    The 'c_ulong' type has a variable size depending on the target platform,
    either u32 or u64. While it could be unconditionally cast to 'u64', this
    results in clippy reporting warnings about unnecessary casts on platforms
    where 'c_ulong' is already a 'u64'. The helper avoids the redundant casts.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- src/util.rs
+++ src/util.rs
@@ -54,6 +54,16 @@ pub(crate) use impl_enum;
 pub(crate) use impl_enum_from;
 pub(crate) use impl_enum_to;
 
+#[cfg(all(target_pointer_width = "64", not(windows)))]
+pub fn c_ulong_to_u64(val: ::libc::c_ulong) -> u64 {
+    val
+}
+
+#[cfg(not(all(target_pointer_width = "64", not(windows))))]
+pub fn c_ulong_to_u64(val: ::libc::c_ulong) -> u64 {
+    val as u64
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
